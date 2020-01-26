d094f8efa4f0eac8092c9f5f2e75e15601518a16
Alisson Medeiros <alisson.medeiros@gmail.com>
2020-01-26 20:13:41 -0300

    Criação de estrutura inicial

---
+++ src/main/java/org/jornadacolaborativa/Calculadora.java
@@ -0,0 +1,4 @@
+package org.jornadacolaborativa;
+
+public class Calculadora {
+}
---
+++ src/test/java/org/jornadacolaborativa/CalculadoraTest.java
@@ -0,0 +1,15 @@
+package org.jornadacolaborativa;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class CalculadoraTest {
+
+    @Test
+    public void teste() {
+        Assertions.fail("");
+
+    }
+
+}
